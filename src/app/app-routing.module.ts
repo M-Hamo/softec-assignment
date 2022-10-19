6cf425964e6e5ccdb1fc5a9e00a527e293b209c3
mohamed.eldeeb <mohamed.eldeeb@talabeyah.com>
2022-10-20 00:53:39 +0200

    feat: Set layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { AppComponent } from "./app.component";
+import { LayoutComponent } from "./layout/layout/layout.component";
+import { NotFoundComponent } from "./pages/not-found.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "shop",
+    pathMatch: "full",
+  },
+  {
+    path: "shop",
+    component: LayoutComponent,
+    children: [
+      {
+        path: "",
+        redirectTo: "products",
+        pathMatch: "full",
+      },
+    ],
+  },
+  {
+    path: "**",
+    component: NotFoundComponent,
+    title: "Not found page",
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  public static Components = [AppComponent, LayoutComponent];
+}

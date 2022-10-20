40b85d42b4bfc65437a94ed8f41c9fc72ed0f5fd
mohamed.eldeeb <mohamed.eldeeb@talabeyah.com>
2022-10-20 03:03:38 +0200

    fix: Fixing bug => Missing import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { HeaderComponent } from "./layout/header/header.component";
 import { LayoutModule } from "./layout/layout.module";
 import { NgxProgressBarModule } from "./ngx-progressbar.module";
 import { SharedModule } from "./shared/shared.module";

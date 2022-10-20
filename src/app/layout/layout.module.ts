194110a8b5e7dc94e6badefa958cc8880982a21e
mohamed.eldeeb <mohamed.eldeeb@talabeyah.com>
2022-10-20 03:49:01 +0200

    feat: Header

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { SharedModule } from "@shared/shared.module";
 import { LayoutComponent } from "./layout/layout.component";
 import { RouterModule } from "@angular/router";
+import { HeaderComponent } from "./header/header.component";
 
-const Components = [LayoutComponent];
+const Components = [LayoutComponent, HeaderComponent];
 
 @NgModule({
   declarations: [...Components],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, SharedModule],
   exports: [...Components],
 })
 export class LayoutModule {}
